c52c96a368af45873aad1e8a3d06c19928eff2d6
Hqmzeh <hamzehhazeem8@gmail.com>
2020-10-28 12:48:24 +0300

    I am not sure what I did here

--- That Mafia Game/Assets/Characters/Main_Character/Scripts/playerMovement.cs
+++ That Mafia Game/Assets/Characters/Main_Character/Scripts/playerMovement.cs
@@ -16,8 +16,8 @@ public class playerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + 2.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 2.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
