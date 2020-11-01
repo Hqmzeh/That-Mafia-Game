10929489051a076286523012a2bf6f4d8ce9abf1
N2FE <omarhazeem2@gmail.com>
2020-11-01 19:13:30 +0300

    Update playerMovement.cs

--- That Mafia Game/Assets/Characters/Main_Character/Scripts/playerMovement.cs
+++ That Mafia Game/Assets/Characters/Main_Character/Scripts/playerMovement.cs
@@ -16,7 +16,7 @@ public class playerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 2.1f * horizontal * Time.deltaTime;
+        position.x = position.x + 2.0f * horizontal * Time.deltaTime;
         position.y = position.y + 2.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
